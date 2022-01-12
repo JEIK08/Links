1ca9760c1160482c740d0991020853bfebf17f39
Jair Suárez <jairjosesuarez8@gmail.com>
2022-01-12 13:40:23 -0500

    feat: URL form and list design

--- src/app/dashboard/profile/profile.component.ts
+++ src/app/dashboard/profile/profile.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
 	selector: 'app-profile',
 	templateUrl: './profile.component.html',
 	styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent { }
+export class ProfileComponent {
+
+	@HostBinding('class') public class: string = 'box';
+
+}
--- src/app/dashboard/url-form/url-form.component.ts
+++ src/app/dashboard/url-form/url-form.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
 	selector: 'app-url-form',
 	templateUrl: './url-form.component.html',
 	styleUrls: ['./url-form.component.scss']
 })
-export class UrlFormComponent { }
+export class UrlFormComponent {
+
+	@HostBinding('class') public class: string = 'box';
+
+}
