815b68fd5ad03609268d0f882fd24220e0e22188
Jair Suárez <jairjosesuarez8@gmail.com>
2022-01-12 12:04:26 -0500

    feat: Dashboard initial layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 	{ path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-	{ path: '**', pathMatch: 'full', redirectTo: 'auth' }
+	{ path: '', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+	{ path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
 @NgModule({
